f41d1f04ebdcc37a5c018127cc3162e5fc7ada9a
Elehunt <elehunt57@gmail.com>
2019-06-16 12:32:00 +0100

    personal homepage updated and html connected

--- personal/urls.py
+++ personal/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.conf.urls import include, url
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('homepage/', views.homepage, name='homepage'),
 ]
--- personal/views.py
+++ personal/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello World")
+#def home(request):
+ #   return HttpResponse("Hello World")
+
+def homepage(request):
+	return render(request, 'personal/homepage.html')
